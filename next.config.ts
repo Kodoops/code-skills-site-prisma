9247eede9a26aff6c46aef4012a16c68cfa99db5
athakim <athakim95@gmail.com>
2025-09-08 09:02:35 +0200

    fix prisma plugin

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import type { NextConfig } from "next";
+//@ts-ignore
+import {PrismaPlugin} from "@prisma/nextjs-monorepo-workaround-plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -10,6 +13,12 @@ const nextConfig: NextConfig = {
                 protocol: 'https',
             }
         ]
+    },
+    webpack: (config, {isServer})=>{
+        if(isServer){
+           config.plugins = [...config.plugins, new PrismaPlugin()]
+        }
+        return config;
     }
 };
 
